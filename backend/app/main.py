7478a47e8f36cdfc876ff5a660abbddcc6942f53
gunung-kim <unge001@naver.com>
2026-05-17 01:56:49 +0900

    초기 도커 설정 및 ci 테스트

---
+++ backend/app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Anti bio",
+    description="Anti bio",
+    version="0.0.1",
+)
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
