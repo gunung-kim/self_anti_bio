356dc6384eb1821120416d2e916e1ae62847464c
gunung-kim <unge001@naver.com>
2026-05-28 03:08:29 +0900

    style : ruff format_2

--- backend/app/services/gmini.py
+++ backend/app/services/gmini.py
@@ -13,9 +13,9 @@ async def analyze_resistance(
         f"투여 농도 : {concentration}ug/mL%\n"
         "위 정보를 바탕으로 대장균의 내성 가능성과 대안 약물을 JSON 형태로 반환해줘. \n"
         "반드기 JSON만 반환하고 마크다운 코드블록 없이 순수한 JSON만 한글로 출력해.\n"
-        '형식 : {'
+        "형식 : {"
         '   "resistance_level":"high/medium/low","reason":"...","alternatives":[...]'
-        '}'
+        "}"
     )
     response = client.models.generate_content(
         model="gemini-2.5-flash",
